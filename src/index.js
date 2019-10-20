857bde71003a3155bec25f6ca0ff338a1e6fecd2
Serj <serj2107@gmail.com>
2019-10-20 15:21:50 +0300

    Added prop types

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app/app';
 
+const listOffers = [
+  `Beautiful &amp; luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+  `Wood and stone place`,
+];
+
 const init = () => {
   ReactDOM.render(
-      <App/>,
+      <App
+        offers={listOffers}
+      />,
       document.querySelector(`#root`)
   );
 };
